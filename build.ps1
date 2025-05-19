f3371f12b281fa804020bb406592fde9fa858825
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2025-05-20 02:32:26 +0800

    fix: 7z will overwrite output file

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 $ErrorActionPreference = 'Stop'
 & $Env:USERPROFILE\.vscode\extensions\joelday.papyrus-lang-vscode-*\pyro\pyro.exe "--input-path" skyrimse.ppj "--game-path" "C:\Games\Steam\steamapps\common\Skyrim Special Edition" 
-7z a -tzip TattoosOfSkyrimImproved.zip interface MCM Scripts Source *.ini TattoosOfSkyrim.esp
+7z a -aoa -tzip TattoosOfSkyrimImproved.zip interface MCM Scripts Source *.ini TattoosOfSkyrim.esp
